4b4a820502682e7a77f47ff5122434cf9cb54484
Jenny Webster <jennyweb@storytime.com>
2023-11-20 19:49:02 +0530

    basic types

---
+++ basic-types.js
@@ -0,0 +1,36 @@
+const number = 1;
+console.log(number, typeof number);
+
+const bigNumber = 2345678911;
+console.log(bigNumber, typeof bigNumber);
+
+const string = 'person';
+console.log(string, typeof string);
+
+const boolean = true;
+console.log(boolean, typeof boolean);
+
+const symbol = '@';
+console.log(symbol, typeof symbol);
+
+let undefinedx = undefined;
+console.log(undefinedx, typeof undefinedx);
+
+const details = {
+  phoneNumber: 1234567891,
+  place: true,
+  name: 'jur',
+  symbol: '!'
+};
+
+console.log(details, typeof details);
+
+function function1(cal){
+  let a = 2;
+      let b = 3
+};
+
+console.log(function1, typeof function1);
+
+const nullValue = null;
+console.log(nullValue, typeof nullValue);
