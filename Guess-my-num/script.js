f437bb3b5ed8202d395687e1bb54f3d1fdbaba8e
Jenny Webster <jennyweb@storytime.com>
2023-12-11 17:35:55 +0530

    project

--- Guess-my-num/script.js
+++ Guess-my-num/script.js
@@ -1,3 +1,22 @@
 "use strict";
 
-console.log(document.querySelector('.message').textContent);
+const number = Math.trunc(Math.random() * 20) +1;
+document.querySelector('.number').textContent = number;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = document.querySelector('.guess').value;
+  console.log(guess);
+
+  if(!guess){
+    document.querySelector('.message').textContent = '⛔ No Number!';
+  } else if (guess === number) {
+    document.querySelector('.message').textContent = 'Correct Number!!🎉';
+  } else if (guess > number){
+    document.querySelector('.message').textContent = 'Too High! 🔴';
+  } else if (guess < number){
+    document.querySelector('.message').textContent = 'Too low! 🔴';
+  }
+
+
+
+});
