6f5156cf2b9ea9127ae3e6dcc6020e66b1666010
Jenny Webster <jennyweb@storytime.com>
2023-12-22 20:11:05 +0530

    prac

--- Examples.js
+++ Examples.js
@@ -368,3 +368,40 @@ function uppercase(str) {
 
 // Log the result of calling uppercase with the input string "the quick brown fox" to the console
 console.log(uppercase("the quick brown fox")); 
+
+//// Write a JavaScript function that accepts a string as a parameter and finds the longest word within the string.
+
+function find_longest_word(str)
+{
+  let array1 = str.match(/\w[a-z]{0,}/gi);
+  let result = array1[0];
+
+  for(let x = 1 ; x < array1.length ; x++)
+  {
+    if(result.length < array1[x].length)
+    {
+    result = array1[x];
+    } 
+  }
+  return result;
+}
+console.log(find_longest_word('Web Development Tutorial'));
+
+// Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
+
+function vowel_count(str1)
+{
+  let vowel_list = 'aeiouAEIOU';
+  let vcount = 0;
+  
+  for(let x = 0; x < str1.length ; x++)
+  {
+    if (vowel_list.indexOf(str1[x]) !== -1)
+    {
+      vcount += 1;
+    }
+  
+  }
+  return vcount;
+}
+console.log(vowel_count("The quick brown fox"));
