e43f26cd2f5e8f2ea9aa41652b7e940142962cd0
Jenny Webster <jennyweb@storytime.com>
2024-01-03 20:09:05 +0530

    prac

--- Recursion.js
+++ Recursion.js
@@ -28,4 +28,47 @@ function factorial(x) {
     
     
     console.log(gcd(5, 9));
+
+    /*
+Write a JavaScript program to get integers in the range (x, y) using recursion.
+
+Example : range(2, 9)
+Expected Output : [3, 4, 5, 6, 7, 8]
+
+*/
+
+let range = function(start_num, end_num) {
+  if (end_num - start_num === 2)
+  {
+  return [start_num + 1];
+  } else {
+  let list = range(start_num, end_num -1);
+  list.push(end_num - 1);
+  return list;
+  }
+  };
+  
+  console.log(range(2, 9));
+
+
+  /*
+Write a JavaScript program to compute the sum of an array of integers.
+
+Example : var array = [1, 2, 3, 4, 5, 6]
+Expected Output : 21
+
+*/
+
+let array_sum = function(my_array) {
+  if (my_array.length === 1)
+  {
+  return my_array[0];
+  } else {
+  return my_array.pop() + array_sum(my_array);
+  }
+  };
+  
+  console.log(array_sum([1,2,3,4,5,6]));
+  
+
     
---
+++ Sorting-Algorithm.js
@@ -0,0 +1,42 @@
+/*
+Write a JavaScript program to sort a list of elements using Quick sort.
+Quick sort is a comparison sort, meaning that it can sort items of any type for which a "less-than" relation (formally, a total order) is defined.
+
+*/
+
+function quick_Sort(origArray) {
+  if(origArray.length <= 1){
+  return origArray;
+  } else {
+  let left = [];
+  let right = [];
+  let newArray = [];
+  let pivot = origArray.pop();
+  let length = origArray.length;
+  
+  for(let i = 0; i < length; i++){
+  if(origArray[1] <= pivot) {
+  left.push(origArray[i]);
+  } else{right.push(origArray[i]);
+        }
+  }
+  
+  return newArray.concat(quick_Sort(left), pivot, quick_Sort(right));
+  }
+  
+  }
+  
+  let myArray = [-45,-70, 10, 80, 55, 99];
+  
+  console.log("Original array: " + myArray);
+  
+  let sortedArray = quick_Sort(myArray);
+  console.log("Sorted array: " + sortedArray);
+  
+  /*
+Output:
+
+'Original array: -45,-70,10,80,55,99'
+'Sorted array: -70,-45,10,80,55,99'
+
+*/
